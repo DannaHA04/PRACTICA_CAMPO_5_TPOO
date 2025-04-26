a8b45d5e33a8945aba6356f5ec9de9d23dcaf8b8
DannaHA04 <huamanalvarezdanna@gmail.com>
2025-04-26 18:22:37 -0500

    feat:Estructura de formulario

--- src/main/java/Registros/Frm_Principal.java
+++ src/main/java/Registros/Frm_Principal.java
@@ -26,18 +26,129 @@ public class Frm_Principal extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
+        jLabel9 = new javax.swing.JLabel();
+        jLabel10 = new javax.swing.JLabel();
+        jLabel11 = new javax.swing.JLabel();
+        jTextField2 = new javax.swing.JTextField();
+        jTextField3 = new javax.swing.JTextField();
+        jTextField4 = new javax.swing.JTextField();
+        jTextField5 = new javax.swing.JTextField();
+        jTextField6 = new javax.swing.JTextField();
+        jComboBox1 = new javax.swing.JComboBox<>();
+        jComboBox2 = new javax.swing.JComboBox<>();
+        jComboBox3 = new javax.swing.JComboBox<>();
+        jTextField7 = new javax.swing.JTextField();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 489, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 394, Short.MAX_VALUE)
-        );
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(null);
+        getContentPane().add(jLabel1);
+        jLabel1.setBounds(454, 6, 0, 0);
+
+        jLabel2.setFont(new java.awt.Font("Showcard Gothic", 0, 18)); // NOI18N
+        jLabel2.setText("GESTIÓN DE PERRITOS");
+        getContentPane().add(jLabel2);
+        jLabel2.setBounds(220, 6, 216, 40);
+
+        jLabel3.setText("Codigo:");
+        getContentPane().add(jLabel3);
+        jLabel3.setBounds(20, 60, 90, 16);
+
+        jLabel4.setText("Nombre:");
+        getContentPane().add(jLabel4);
+        jLabel4.setBounds(20, 90, 90, 16);
+
+        jLabel5.setText("Raza:");
+        getContentPane().add(jLabel5);
+        jLabel5.setBounds(20, 110, 50, 30);
+
+        jLabel6.setText("Tamaño:");
+        getContentPane().add(jLabel6);
+        jLabel6.setBounds(20, 210, 80, 20);
+
+        jLabel7.setText("Peso:");
+        getContentPane().add(jLabel7);
+        jLabel7.setBounds(20, 180, 60, 20);
+
+        jLabel8.setText("Edad:");
+        getContentPane().add(jLabel8);
+        jLabel8.setBounds(20, 150, 60, 20);
+
+        jLabel9.setText("Genero:");
+        getContentPane().add(jLabel9);
+        jLabel9.setBounds(20, 240, 80, 20);
+
+        jLabel10.setText("Estado de Salud:");
+        getContentPane().add(jLabel10);
+        jLabel10.setBounds(20, 266, 100, 30);
+
+        jLabel11.setText("Fecha de ingreso:");
+        getContentPane().add(jLabel11);
+        jLabel11.setBounds(20, 300, 140, 20);
+        getContentPane().add(jTextField2);
+        jTextField2.setBounds(70, 60, 160, 22);
+        getContentPane().add(jTextField3);
+        jTextField3.setBounds(80, 90, 150, 22);
+        getContentPane().add(jTextField4);
+        jTextField4.setBounds(60, 120, 170, 22);
+        getContentPane().add(jTextField5);
+        jTextField5.setBounds(120, 300, 100, 22);
+        getContentPane().add(jTextField6);
+        jTextField6.setBounds(60, 180, 90, 22);
+
+        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { ">>>SELECCIONA<<<", "Saludable", "En tratamiento", "Recuperación", "Crítico" }));
+        getContentPane().add(jComboBox1);
+        jComboBox1.setBounds(120, 270, 160, 22);
+
+        jComboBox2.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { ">>>SELECCIONA<<<", "Pequeño", "Mediano", "Grande" }));
+        getContentPane().add(jComboBox2);
+        jComboBox2.setBounds(80, 210, 160, 22);
+
+        jComboBox3.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { ">>>SELECCIONA<<<", "Macho", "Hembra" }));
+        getContentPane().add(jComboBox3);
+        jComboBox3.setBounds(80, 240, 160, 22);
+        getContentPane().add(jTextField7);
+        jTextField7.setBounds(60, 150, 90, 22);
+
+        jButton1.setText("Limpiar");
+        getContentPane().add(jButton1);
+        jButton1.setBounds(20, 350, 80, 30);
+
+        jButton2.setText("Agregar");
+        getContentPane().add(jButton2);
+        jButton2.setBounds(120, 350, 90, 30);
+
+        jButton3.setText("Salir");
+        getContentPane().add(jButton3);
+        jButton3.setBounds(70, 390, 90, 30);
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane1.setViewportView(jTable1);
+
+        getContentPane().add(jScrollPane1);
+        jScrollPane1.setBounds(300, 50, 452, 360);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -78,5 +189,30 @@ public class Frm_Principal extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JComboBox<String> jComboBox1;
+    private javax.swing.JComboBox<String> jComboBox2;
+    private javax.swing.JComboBox<String> jComboBox3;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel10;
+    private javax.swing.JLabel jLabel11;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTable1;
+    private javax.swing.JTextField jTextField2;
+    private javax.swing.JTextField jTextField3;
+    private javax.swing.JTextField jTextField4;
+    private javax.swing.JTextField jTextField5;
+    private javax.swing.JTextField jTextField6;
+    private javax.swing.JTextField jTextField7;
     // End of variables declaration//GEN-END:variables
 }
