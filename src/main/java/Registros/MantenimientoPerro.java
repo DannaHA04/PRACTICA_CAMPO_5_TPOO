15f362b5e3232755c1a0f9a7ed8f03347a81bf40
DannaHA04 <huamanalvarezdanna@gmail.com>
2025-04-26 19:26:10 -0500

    feat:Uso de colecciones y sobrecarga de metodos

--- src/main/java/Registros/MantenimientoPerro.java
+++ src/main/java/Registros/MantenimientoPerro.java
@@ -1,6 +1,36 @@
 
 package Registros;
 
+import java.util.ArrayList;
+
 public class MantenimientoPerro {
     
+    //USO DE COLECCIONES
+    private ArrayList<Perro> listaPerros;
+
+    // Constructor
+    public MantenimientoPerro() {
+        
+        //Inicializa la coleccion
+        listaPerros = new ArrayList<>();
+    }
+    
+    //USO DE METODO SOBRECARGADO
+
+    // Método sobrecargado para agregar un solo perro
+    public void agregarPerro(Perro perro) {
+        listaPerros.add(perro);  // Agregar un perro a la lista
+    }
+
+    // Método sobrecargado para agregar varios perros
+    public void agregarPerro(Perro[] perros) {
+        for(Perro perro : perros) {
+            listaPerros.add(perro); // Agregar cada perro a la lista
+        }
+    }
+
+    // Método para obtener todos los perros
+    public ArrayList<Perro> obtenerPerros() {
+        return listaPerros;
+    }
 }
